fce6ed8903271999f754bb2e5f04c40f2e1b0bec
Nikhil Srivastav <44610117+niksrivastav@users.noreply.github.com>
2020-05-19 16:45:30 +0530

    Add files via upload

---
+++ camel-java-dsl/camel-java-dsl/src/main/java/my/project/Application.java
@@ -0,0 +1,21 @@
+package my.project;
+
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+    public ServletRegistrationBean camelServletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(),"/camel/*");
+        registration.setName("CamelServlet");
+        return registration;
+    }
+}
