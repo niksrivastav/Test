69967f168a6f28f0ea8ca7ae8b060e339f4f4c73
Nikhil Srivastav <44610117+niksrivastav@users.noreply.github.com>
2020-05-24 12:52:14 +0530

    Add files via upload

--- Practice1/src/main/java/my/project/roue/MyRoute.java
+++ Practice1/src/main/java/my/project/roue/MyRoute.java
@@ -4,35 +4,30 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
 
-import my.project.model.ResponseObject;
-
 @Component
 public class MyRoute extends RouteBuilder{
 
 	@Override
-	public void configure() throws Exception {
-		
+    public void configure() throws Exception {
+
         restConfiguration()
-          .component("servlet")
-          .bindingMode(RestBindingMode.json);
+                .bindingMode(RestBindingMode.auto)
+                .component("servlet");
 
-        // REST DSL with a single GET /hello service
         rest()
-          .get("/hello")
-    	      .to("direct:hello");
+                .path("/hello")
 
-        // route called from REST service that builds a response message
-        from("direct:hello")
-          .log("Hello World")
-          .bean(this, "createResponse");
-		
-	}
-	
-	public ResponseObject createResponse() {
-        ResponseObject response = new ResponseObject();
-        response.setResponse("Hello World");
-        response.setName("Nikkhil");
-        return response;
+                .get("/")
+                .route()
+                .transform(simple("Good Morning"))
+                .endRest()
+
+                .post("/")
+                .route()
+                .to("log:mylogger?showAll=true")
+                .transform(simple("Welcome: ${body[name]}"))
+                .endRest();
+
     }
 
 }
